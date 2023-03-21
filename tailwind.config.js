af55c1fac08eb65a67181ddab7a11b28fbb20e17
Lauwi Meara <laurasusanne@live.nl>
2023-03-21 19:35:20 +0100

    Add gradient to bottom padding of the category bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,8 @@ module.exports = {
       backgroundImage: (theme) => ({
         'gradient-recappt-white': 'linear-gradient(to bottom right, #f5f5f5, #f1f5f9, #cbd5e1)',
         'gradient-amber-100': 'linear-gradient(to bottom right, #fffbeb, #fef3c7)',
-        'gradient-amber-300': 'linear-gradient(to bottom right, #fde68a, #fcd34d)'
+        'gradient-amber-300': 'linear-gradient(to bottom right, #fde68a, #fcd34d)',
+        'gradient-recappt-gray-to-transparent': 'linear-gradient(to bottom, rgba(119,136,153, 1), rgba(119,136,153, 1), rgba(119,136,153, 1), rgba(119,136,153, 1), rgba(0,0,0,0) 100%)'
       }),
     },
   },
