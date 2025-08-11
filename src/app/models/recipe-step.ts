97f0610bf234139743b8e103085843b4fdf8dd9b
Lauwi Meara <laurasusanne@live.nl>
2025-08-11 15:26:19 +0200

    fix json keys with underscores

--- src/app/models/recipe-step.ts
+++ src/app/models/recipe-step.ts
@@ -5,20 +5,3 @@ export interface RecipeStep {
     description: string;
     isActive: boolean;
 }
-
-export interface RecipeStepJSON {
-    id: number;
-    recipe_id: number;
-    number: number;
-    description: string;
-}
-
-export function jsonToRecipeStep(json: RecipeStepJSON): RecipeStep {
-    return {
-        id: json.id,
-        recipeId: json.recipe_id,
-        number: json.number,
-        description: json.description,
-        isActive: false
-    }
-}
