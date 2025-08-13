0654f286e3892754e75f009c09153dd0a93e6f58
Lauwi Meara <laurasusanne@live.nl>
2025-08-13 16:05:12 +0200

    change colors recipe detail

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'recappt-light-gray': '#c8dfdf',
         'recappt-gray': '#437272',
         'recappt-dark-gray': '#2f4f4f',
         'recappt-white': '#f5f5f5',
@@ -18,9 +19,9 @@ module.exports = {
         '-flip': '-1'
       },
       backgroundImage: (theme) => ({
-        'gradient-recappt-white': 'linear-gradient(to bottom right, #f5f5f5, #f1f5f9, #cbd5e1)',
-        'gradient-recappt-dark-gray': 'linear-gradient(to bottom right, #396060, #2f4f4f)',
+        'gradient-recappt-white': 'linear-gradient(to bottom right, #f5f5f5, #c8dfdf, #c8dfdf, #c8dfdf, #99c4c4)',
         'gradient-recappt-gray': 'linear-gradient(to bottom right, #5fa0a0, #437272)',
+        'gradient-recappt-dark-gray': 'linear-gradient(to bottom right, #396060, #2f4f4f)',
         'gradient-recappt-dark-gray-to-transparent': 'linear-gradient(to bottom, rgba(47, 79, 79, 1), rgba(47, 79, 79, 1), rgba(47, 79, 79, 1), rgba(47, 79, 79, 1), rgba(0,0,0,0) 100%)'
       }),
     },
